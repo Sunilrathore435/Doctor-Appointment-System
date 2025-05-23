4ec4c1c3bf163413d53660eb52fb777e43abe423
Sunil Rathore <154366337+Sunilrathore435@users.noreply.github.com>
2025-05-23 15:58:12 +0530

    cursor

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -60,12 +60,12 @@ const Navbar = () => {
         // :<button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font light hidden md:block'> Create account</button>
            :<button onClick={()=>navigate('/login')} className=' text-black border  px-8 py-3 rounded-full font light hidden hover:bg-primary hover:text-white md:block'> LOGIN </button>
 }
-<img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+<img onClick={()=>setShowMenu(true)} className='w-6 cursor-pointer md:hidden' src={assets.menu_icon} alt="" />
 {/*------- Mobile Menu ------- */}
 <div className={` ${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
     <div className='flex items-center justify-between px-5 py-6'>
         <img className='w-36' src={assets.logo} alt="" />
-        <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+        <img className='w-7 cursor-pointer' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
     </div>
     <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium onClick={}'>
         <NavLink  onClick={()=>setShowMenu(false)} to='/'><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
