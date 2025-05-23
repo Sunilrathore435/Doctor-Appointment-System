4ee185057632d658a1376cc7b1c1e415bd16e9a7
Sunil Rathore <154366337+Sunilrathore435@users.noreply.github.com>
2025-05-23 20:40:21 +0530

    Doctor Animations

--- frontend/src/components/LoadingAnimation.jsx
+++ frontend/src/components/LoadingAnimation.jsx
@@ -3,13 +3,17 @@ import Lottie from 'lottie-react'
 import livechat from '../assets/livechat.json'  // Adjust the path if needed
 
 const LoadingAnimation = () => {
-    return (
-    <Lottie 
-      animationData={livechat} 
-      loop={true} 
-      style={{ width: 200, height: 200, margin: 'auto' }} 
-    />
+  return (
+    <div className="flex flex-col items-center justify-center gap-3 p-4">
+      <p className="text-lg font-semibold text-gray-700 animate-pulse">
+      Our <span className="text-red-600 font-bold">Doctors</span> are loading, please wait...
+      </p>
+      <div className="w-40 h-40">
+        <Lottie animationData={livechat} loop={true} />
+      </div>
+    </div>
   )
 }
+
 
 export default LoadingAnimation
