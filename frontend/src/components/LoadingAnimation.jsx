0da3a29bd5a962cf07c2cee225aaf933643846f6
Sunil Rathore <154366337+Sunilrathore435@users.noreply.github.com>
2025-05-23 20:42:49 +0530

    Refine

--- frontend/src/components/LoadingAnimation.jsx
+++ frontend/src/components/LoadingAnimation.jsx
@@ -5,9 +5,9 @@ import livechat from '../assets/livechat.json'  // Adjust the path if needed
 const LoadingAnimation = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-3 p-4">
-      <p className="text-lg font-semibold text-gray-700 animate-pulse">
-      Our <span className="text-red-600 font-bold">Doctors</span> are loading, please wait...
-      </p>
+    <p className="text-center text-xl sm:text-2xl font-semibold text-gray-700 animate-pulse">
+  Please hold on while we load <span className="text-red-600 font-bold">our doctors</span> for you...
+</p>
       <div className="w-40 h-40">
         <Lottie animationData={livechat} loop={true} />
       </div>
