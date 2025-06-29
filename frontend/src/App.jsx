e1158fa444c280c8bcb7e37125f11c82654c2723
Sunil Rathore <154366337+Sunilrathore435@users.noreply.github.com>
2025-06-30 01:05:24 +0530

    Analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,27 +13,28 @@ import Footer from './components/Footer'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ScrollToTop from './components/ScrollToTop'
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-      <ToastContainer/>
+      <ToastContainer />
       <Navbar />
-      <ScrollToTop/>
+      <ScrollToTop />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/doctors' element={<Doctor/>} />
-        <Route path='/doctors/:speciality' element={<Doctor/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/my-profile' element={<MyProfile/>} />
-        <Route path='/my-appointments' element={<MyAppointments/>} />
-        <Route path='/appointment/:docId' element={<Appointment/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctor />} />
+        <Route path='/doctors/:speciality' element={<Doctor />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
-      <Footer/>
+      <Footer />
+      <Analytics />
 
-     
     </div>
   )
 }
